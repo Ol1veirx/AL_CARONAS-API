b0290f4f181e530ae70b98ce86df3766529a2139
Oliveira <liedsonwork@hotmail.com>
2024-03-12 10:24:37 -0300

    third commit

--- AL_CARONAS.Core/Entities/Review.cs
+++ AL_CARONAS.Core/Entities/Review.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace AL_CARONAS.Core.Entities
         public int ReviewId { get; set; }
         public int UserId { get; set; }
         public int TargerUserId { get; set; }
+
+        [Range(0, 5)]
         public int Rating { get; set; }
         public string? Comment { get; set; }
         public DateTime Date {  get; set; } = DateTime.Now;
